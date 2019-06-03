8972f56864be08597969e58bae3fe75ca782bd47
shanzzo <50712261+shanzzo@users.noreply.github.com>
2019-06-03 15:13:32 +0800

    Update StaffController.java

--- StaffController.java
+++ StaffController.java
@@ -6,7 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import javax.servlet.RequestDispatcher;
 import meylis.dao.StaffDAO;
 import meylis.model.StaffBean;
 
@@ -16,40 +16,62 @@ import meylis.model.StaffBean;
 @WebServlet("/StaffController")
 public class StaffController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	 private static String ADD = "/addStaff.jsp";
-	    //private static String UPDATE = "/updateProduct.jsp";
-	    //private static String LIST_ALL = "/listProduct.jsp";
-	    private StaffDAO dao;
-       
-   
-    public StaffController() {
-        super();
-        dao = new StaffDAO();
+	private static String ADD = "/addStaff.jsp";
+	private static String LIST = "/listStaff.jsp";
+	private static String UPDATE = "/updateStaff.jsp";
+	private static String LOGIN = "/login.jsp";
+	private StaffDAO d;
+
+
+	public StaffController() {
+		super();
+		d = new StaffDAO();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String forward="";
+        String action = request.getParameter("action");
+
+        if (action.equalsIgnoreCase("listAll")){
+        	forward = LIST;
+        	request.setAttribute("staff", d.getAllStaff());
+        }
+        if (action.equalsIgnoreCase("delete")){
+    	    String id = (request.getParameter("id"));
+            d.deleteStaff(id);
+            forward = LIST;
+            request.setAttribute("staff", d.getAllStaff());    
     }
 
-	
+        RequestDispatcher view = request.getRequestDispatcher(forward);
+        view.forward(request, response);
+	}
 
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		StaffBean staff = new StaffBean();
-		staff.setId(request.getParameter("id"));
 		staff.setName(request.getParameter("name"));
-		staff.setEmail(request.getParameter("email"));
-		staff.setDepartment(request.getParameter("department"));
 		staff.setAge(Integer.parseInt(request.getParameter("age")));
+		staff.setDepartment(request.getParameter("department"));
+		staff.setEmail(request.getParameter("email"));
+		staff.setAddress(request.getParameter("address"));		
 		staff.setPassword(request.getParameter("password"));
-		staff.setAddress(request.getParameter("address"));
+		staff.setGrade(request.getParameter("grade"));
 
 		String Id = request.getParameter("id");
 		if(Id == null || Id.isEmpty()){
-	        dao.add(staff);
-	    }
-	    /*else{
+			d.add(staff);
+		}
+	    /*else if(Id ){
 	         staff.setId(Id);
 	         dao.updateStaff(staff);
-	   }*/
-	        response.sendRedirect("login.jsp");
-	}
+	     }*/
+	     response.sendRedirect("listStaff.jsp");
+	 }
 
 	}
-
-
