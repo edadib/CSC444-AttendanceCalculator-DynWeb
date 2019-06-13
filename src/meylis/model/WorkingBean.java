4a5468ddda4f250d54f2c1aa21eca886524117ff
Adib Farhan <adib.farhan97@gmail.com>
2019-06-14 00:26:28 +0800

    Add files via upload

---
+++ src/meylis/model/WorkingBean.java
@@ -0,0 +1,86 @@
+package meylis.model;
+
+
+public class WorkingBean {
+	int W_ID;
+	float mon;
+	float tue;
+	float wed;
+	float thu;
+	float fri;
+	float sat;
+	float sun;
+	float total;
+	String day,Comments;
+	
+	public String getComments() {
+		return Comments;
+	}
+	public void setComments(String comments) {
+		Comments = comments;
+	}
+	public float getTotal() {
+		return total;
+	}
+	public void setTotal(float total) {
+		this.total = total;
+	}
+	public String getDay() {
+		return day;
+	}
+	public void setDay(String day) {
+		this.day = day;
+	}
+	public int getW_ID() {
+		return W_ID;
+	}
+	public void setW_ID(int w_ID) {
+		W_ID = w_ID;
+	}
+	public float getMon() {
+		return mon;
+	}
+	public void setMon(float mon) {
+		this.mon = mon;
+	}
+	public float getTue() {
+		return tue;
+	}
+	public void setTue(float tue) {
+		this.tue = tue;
+	}
+	public float getWed() {
+		return wed;
+	}
+	public void setWed(float wed) {
+		this.wed = wed;
+	}
+	public float getThu() {
+		return thu;
+	}
+	public void setThu(float thu) {
+		this.thu = thu;
+	}
+	public float getFri() {
+		return fri;
+	}
+	public void setFri(float fri) {
+		this.fri = fri;
+	}
+	public float getSat() {
+		return sat;
+	}
+	public void setSat(float sat) {
+		this.sat = sat;
+	}
+	public float getSun() {
+		return sun;
+	}
+	public void setSun(float sun) {
+		this.sun = sun;
+	}
+	
+	
+	
+}
+	
